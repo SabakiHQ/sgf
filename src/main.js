1d4bd9d4e6695fc56929a4d38c2e4f3bd852c570
Yichuan Shen <shen.yichuan@gmail.com>
2019-01-12 02:40:06 +0100

    Export generator tokenizers

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-const {tokenize, tokenizeBuffer} = require('./tokenize')
+const {tokenizeIter, tokenizeBufferIter, tokenize, tokenizeBuffer} = require('./tokenize')
 const {parse, parseTokens, parseBuffer, parseFile} = require('./parse')
 const {stringify} = require('./stringify')
 const helper = require('./helper')
 
 Object.assign(exports, {
+    tokenizeIter,
+    tokenizeBufferIter,
     tokenize,
     tokenizeBuffer,
     parse,
